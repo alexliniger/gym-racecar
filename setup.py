8e807870b248bf57697f98c56fc4980787b7fe5d
alexliniger <alexliniger@gmail.com>
2020-11-20 14:28:37 +0100

    typo fixed

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name='gym_racecar',
       version='0.0.1',
       install_requires=['gym>=0.17.3',
-                        'numpy>=1.10.4'
+                        'numpy>=1.10.4',
                         'math']
 )
