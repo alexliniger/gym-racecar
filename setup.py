77dc0aa408698369376fd1b15bec3e2f0aa3b5bd
alexliniger <alexliniger@gmail.com>
2020-11-20 14:36:26 +0100

    req change

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ from setuptools import setup
 setup(name='gym_racecar',
       version='0.0.1',
       install_requires=['gym>=0.17.3',
-                        'numpy>=1.10.4',
-                        'math']
+                        'numpy>=1.19.2']
 )
